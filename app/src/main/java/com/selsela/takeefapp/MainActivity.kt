695f0212451b2ba5d53189afbdcedb36199c66ca
qamarsafadi1 <q.safadi@selsela.net>
2023-01-04 11:17:12 +0200

    feat: init tap bar

--- app/src/main/java/com/selsela/takeefapp/MainActivity.kt
+++ app/src/main/java/com/selsela/takeefapp/MainActivity.kt
@@ -1,25 +1,46 @@
 package com.selsela.takeefapp
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalLayoutDirection
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.LayoutDirection
+import androidx.navigation.compose.rememberNavController
+import com.selsela.takeefapp.navigation.Destinations
 import com.selsela.takeefapp.navigation.NavigationHost
+import com.selsela.takeefapp.ui.splash.ChangeStatusBarColor
 import com.selsela.takeefapp.ui.splash.SplashView
+import com.selsela.takeefapp.ui.theme.SecondaryColor
 import com.selsela.takeefapp.ui.theme.TakeefAppTheme
+import com.selsela.takeefapp.ui.theme.text14Meduim
+import com.selsela.takeefapp.utils.Extensions.Companion.bindToolbarTitle
+import com.selsela.takeefapp.utils.Extensions.Companion.log
+import com.selsela.takeefapp.utils.Extensions.Companion.showingBackButton
 import com.selsela.takeefapp.utils.LocalData
 
 class MainActivity : ComponentActivity() {
+    @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
+    @OptIn(ExperimentalMaterial3Api::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
@@ -28,13 +49,64 @@ class MainActivity : ComponentActivity() {
                     LocalLayoutDirection provides
                             if (LocalData.appLocal == "ar") LayoutDirection.Rtl
                             else LayoutDirection.Ltr
-                ){
-                    Surface(
-                        modifier = Modifier.fillMaxSize(),
-                        color = Color.White
+                ) {
+                    val navController = rememberNavController()
+                    val currentRoute = navController.currentBackStackEntryFlow.collectAsState(
+                        initial = navController.currentBackStackEntry
+                    )
+                    Scaffold(
+                        topBar = {
+                            if (currentRoute.value?.destination?.route != Destinations.HOME_SCREEN && currentRoute.value?.destination?.route != Destinations.SPLASH_SCREEN) {
+                                Color.White.ChangeStatusBarColor()
+                                TopAppBar(
+                                    title = {
+                                        currentRoute.value?.let {
+                                            val title =
+                                                navController.bindToolbarTitle(currentRoute = it)
+                                            Text(
+                                                text = title,
+                                                style = text14Meduim,
+                                                modifier = Modifier.fillMaxWidth(),
+                                                textAlign = TextAlign.Center
+                                            )
+                                        }
+                                    },
+                                    navigationIcon = {
+
+                                        currentRoute.value?.let {
+                                            val isShowing = navController.showingBackButton(
+                                                it
+                                            )
+                                            if (isShowing) {
+                                                IconButton(onClick = {
+                                                    navController.navigateUp()
+                                                }) {
+                                                    Icon(
+                                                        painter = painterResource(id = R.drawable.backbutton),
+                                                        contentDescription = "",
+                                                        tint = SecondaryColor
+                                                    )
+                                                }
+
+                                            }
+                                        }
+
+                                    },
+                                    colors = TopAppBarDefaults.mediumTopAppBarColors(
+                                        containerColor = Color.White
+                                    )
+                                )
+                            }
+                        }
                     ) {
-                        NavigationHost()
+                        Surface(
+                            modifier = Modifier.fillMaxSize(),
+                            color = Color.White
+                        ) {
+                            NavigationHost(navController)
+                        }
                     }
+
                 }
             }
         }
--- app/src/main/java/com/selsela/takeefapp/utils/Extensions.kt
+++ app/src/main/java/com/selsela/takeefapp/utils/Extensions.kt
@@ -5,8 +5,13 @@ import android.os.Handler
 import android.os.Looper
 import android.util.Log
 import androidx.core.content.ContextCompat
+import androidx.navigation.NavBackStackEntry
 import androidx.navigation.NavController
 import androidx.navigation.NavOptionsBuilder
+import com.selsela.takeefapp.navigation.Destinations
+import java.text.DecimalFormat
+import java.text.DecimalFormatSymbols
+import java.util.Locale
 
 class Extensions {
     companion object{
@@ -16,6 +21,13 @@ class Extensions {
         fun Context.findDrawable(color: Int): Int {
             return ContextCompat.getColor(this, color)
         }
+
+        fun Int.convertToDecimalPatter(): String{
+           return DecimalFormat(
+                "00",
+                DecimalFormatSymbols(Locale.US)
+            ).format(this)
+        }
         fun Any.log(tag: String = "") {
             if (tag.equals("")) {
                 Log.d("QMR : ", this.toString())
@@ -23,6 +35,20 @@ class Extensions {
                 Log.d("QMR$tag", this.toString())
 
             }
+        }
+        fun NavController.bindToolbarTitle(currentRoute: NavBackStackEntry): String{
+            var title =  when(currentRoute.destination.route){
+                Destinations.HOME_SCREEN,Destinations.LOGIN_SCREEN -> ""
+                else -> "Selsela"
+            }
+            return title
+        }
+        fun NavController.showingBackButton(currentRoute: NavBackStackEntry): Boolean{
+            var showBackButton =  when(currentRoute.destination.route){
+                Destinations.HOME_SCREEN,Destinations.SPLASH_SCREEN -> false
+                else -> true
+            }
+            return showBackButton
         }
         fun NavOptionsBuilder.navigateWithClearBackStack(navController: NavController) {
             popUpTo(navController.currentBackStackEntry?.destination?.route ?: return) {
